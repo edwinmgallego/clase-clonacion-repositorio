ad42c62795b13b2ccabbee54d18717e126eb5fa6
Usuario <Usuario@11.11.3.114>
2026-09-16 10:58:06 -0500

    Nuevo cambio

---
+++ JuanBedoya/src/main/java/com/mycompany/juanbedoya/JuanBedoya.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juanbedoya;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JuanBedoya {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World #2!");
+    }
+}
